b543999d76ce41227d7ddbac629d2c3aaae25e2c
Manish Madeshia <162539437+manishmadeshia06@users.noreply.github.com>
2025-08-19 22:55:09 +0530

    division function added

--- src/main/java/mypackage/MyCalculator.java
+++ src/main/java/mypackage/MyCalculator.java
@@ -15,12 +15,18 @@ public class MyCalculator {
 	{
 		return a*b;
 	}
+	public int div(int a, int b) 
+	{
+		return a/b;
+	}
 	
 	public static void main(String[] args) {
 		MyCalculator calc = new MyCalculator();
 		System.out.println("sum is "+ calc.sum(10,20));
-		System.out.println("sum is "+ calc.diff(10,20));
-		System.out.println("sum is "+ calc.mult(10,20));
+		System.out.println("diff is "+ calc.diff(10,20));
+		System.out.println("mult is "+ calc.mult(10,20));
+		System.out.println("div is "+ calc.div(10,20));
+
 
 	}
 }
