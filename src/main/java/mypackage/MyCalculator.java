f5544031e774d430a96d059201fbfabbb546d786
Manish Madeshia <162539437+manishmadeshia06@users.noreply.github.com>
2025-08-19 22:40:10 +0530

    mult () function added

--- src/main/java/mypackage/MyCalculator.java
+++ src/main/java/mypackage/MyCalculator.java
@@ -11,11 +11,16 @@ public class MyCalculator {
 		return a-b;
 	}
 	
+	public int mult(int a, int b) 
+	{
+		return a*b;
+	}
 	
 	public static void main(String[] args) {
 		MyCalculator calc = new MyCalculator();
 		System.out.println("sum is "+ calc.sum(10,20));
 		System.out.println("sum is "+ calc.diff(10,20));
+		System.out.println("sum is "+ calc.mult(10,20));
 
 	}
 }
